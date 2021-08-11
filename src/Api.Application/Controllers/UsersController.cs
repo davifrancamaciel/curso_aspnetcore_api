eb9daac5c83d6acc8434656868147b9da6f9147c
Davi França Maciel <davifrancamaciel@hotmail.com>
2021-08-11 18:53:00 -0300

    configurando swagger

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Policy;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -31,6 +33,88 @@ namespace Api.Application.Controllers
             try
             {
                 return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError,
+                e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetWithId")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError,
+                e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UserEntity request)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                var result = await _service.Post(request);
+                if (result != null)
+                    return Created(new Uri(Url
+                            .Link("GetWithId", new { id = result.Id })),
+                    result);
+                else
+                    return BadRequest();
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError,
+                e.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] UserEntity request)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                var result = await _service.Put(request);
+                if (result != null)
+                    return Ok(result);
+                else
+                    return BadRequest();
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError,
+                e.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Delete(id));
             }
             catch (ArgumentException e)
             {
--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.CrossCutting.DependencyInjection;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -29,6 +31,30 @@ namespace application
             ConfigureService.ConfigureDependenciesService (services);
             ConfigureRepository.ConfigureDependenciesRepository (services);
             services.AddControllers();
+            services
+                .AddSwaggerGen(x =>
+                {
+                    x
+                        .SwaggerDoc("v1",
+                        new OpenApiInfo {
+                            Version = "v1",
+                            Title = "Curso de API com AspNetCore 3.1",
+                            Description = "Arquitetura de Api DDD",
+                            TermsOfService =
+                                new Uri("http://www.mfrinfo.com.br"),
+                            Contact =
+                                new OpenApiContact {
+                                    Name = "Davi França Maciel",
+                                    Email = "davifrancamaciel@gmail.com",
+                                    Url = new Uri("http://www.mfrinfo.com.br")
+                                },
+                            License =
+                                new OpenApiLicense {
+                                    Name = "Termo de licença de uso",
+                                    Url = new Uri("http://www.mfrinfo.com.br")
+                                }
+                        });
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +64,16 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app
+                .UseSwaggerUI(x =>
+                {
+                    x
+                        .SwaggerEndpoint("/swagger/v1/swagger.json",
+                        "Curso de API com AspNetCore 3.1");
+                    x.RoutePrefix = string.Empty;
+                });
 
             app.UseRouting();
 
